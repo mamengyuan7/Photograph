2b73ed9c28a3a45049e9f5fac14380f1fc53a310
mamengyuan7 <3023923835@qq.com>
2021-02-08 15:10:20 +0800

    m

--- photographGet/app/src/main/java/net/onest/photographget/DetailedActivity.java
+++ photographGet/app/src/main/java/net/onest/photographget/DetailedActivity.java
@@ -1,8 +1,14 @@
 package net.onest.photographget;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -14,18 +20,29 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.blankj.utilcode.util.ConvertUtils;
+import com.bumptech.glide.Glide;
+import com.hitomi.tilibrary.style.index.NumberIndexIndicator;
+import com.hitomi.tilibrary.style.progress.ProgressBarIndicator;
+import com.hitomi.tilibrary.transfer.TransferConfig;
+import com.vansz.glideimageloader.GlideImageLoader;
 import com.wx.goodview.GoodView;
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
 
 import net.onest.photographget.adapter.AdapterComment;
 import net.onest.photographget.model.Comment;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class DetailedActivity extends AppCompatActivity implements View.OnClickListener {
+public class DetailedActivity extends BaseActivity implements View.OnClickListener {
     private MultiImageView multiImageView;
     private GoodView mGoodView;
 
@@ -38,16 +55,152 @@ public class DetailedActivity extends AppCompatActivity implements View.OnClickL
     private RelativeLayout rl_comment;
 
     private ListView comment_list;
-    private AdapterComment adapterComment;
     private List<Comment> data;
+    private AdapterComment adapterComment;
+
+
+    private List<String> imgs = new ArrayList<>();
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_detailed);
+    protected int getContentView() {
+        return R.layout.enlarge_view;
 
-        //图片展示
-        multiImageView = findViewById(R.id.patient_detail_table_pictures);
+    }
 
+    @Override
+    protected void initView() {
+        RecyclerView rvImages = findViewById(R.id.rv_images);
+        rvImages.setLayoutManager(new LinearLayoutManager(this));
+        rvImages.setAdapter(new FriendsCircleAdapter());
+    }
+
+    @Override
+    protected void testTransferee() {
+    }
+
+    private TransferConfig.Builder getBuilder(int pos) {
+        TransferConfig.Builder builder = TransferConfig.build()
+                .setProgressIndicator(new ProgressBarIndicator())
+                .setIndexIndicator(new NumberIndexIndicator())
+                .setImageLoader(GlideImageLoader.with(getApplicationContext()));
+        if (pos == 4) {
+            builder.enableHideThumb(false);
+        } else if (pos == 5) {
+            builder.enableJustLoadHitPage(true);
+        } else if (pos == 6) {
+            builder.enableDragPause(true);
+        }
+        return builder;
+    }
+
+    /**
+     * 朋友圈列表数据适配器
+     */
+    private class FriendsCircleAdapter extends CommonAdapter<Pair<String, List<String>>> {
+        private DividerGridItemDecoration divider = new DividerGridItemDecoration(
+                Color.TRANSPARENT,
+                ConvertUtils.dp2px(8f),
+                ConvertUtils.dp2px(8f)
+        );
+
+        FriendsCircleAdapter() {
+            super(DetailedActivity.this, R.layout.home_detailed, getFriendsCircleList(DetailedActivity.this));
+        }
+
+        @Override
+        protected void convert(ViewHolder viewHolder, final Pair<String, List<String>> item, final int position) {
+            //详情
+            viewHolder.setText(R.id.de_type, item.first);
+            //题目
+            viewHolder.setText(R.id.de_title,"郊游");
+            //头像
+            Drawable drawable = getResources().getDrawable(R.mipmap.b1);
+            BitmapDrawable bd = (BitmapDrawable) drawable;
+            final Bitmap bmm = bd.getBitmap();
+            viewHolder.setImageBitmap(R.id.de_touxiang,bmm);
+            //点赞
+            viewHolder.setOnClickListener(R.id.collection, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("iiii","999999999999999999999999999999999999999999999999999999999999");
+                    mGoodView = new GoodView(DetailedActivity.this);
+                    Drawable drawable = getResources().getDrawable(R.drawable.collection_checked);
+                    BitmapDrawable bd = (BitmapDrawable) drawable;
+                    final Bitmap bm = bd.getBitmap();
+                    viewHolder.setImageBitmap(R.id.collection,bm);
+                    mGoodView.setTextInfo("收藏成功", Color.parseColor("#f66467"), 12);
+                    mGoodView.show(viewHolder.getConvertView());
+                }
+            });
+            //评论
+            viewHolder.setOnClickListener(R.id.comment, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.e("aaaaa","55555555555555555555555555555555555555555555555555555");
+                    // 弹出输入法
+                    InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                    // 显示评论框
+                    rl_enroll = (LinearLayout) viewHolder.getConvertView();
+                    //comment_content = (EditText) viewHolder.getConvertView();
+                    //comment_send = (Button) viewHolder.getConvertView();
+                    //hide_down = (TextView) viewHolder.getConvertView();
+                    rl_enroll.setVisibility(View.GONE);
+                    //rl_comment = (RelativeLayout) viewHolder.getConvertView();
+                    //rl_comment.setVisibility(View.VISIBLE);
+                }
+            });
+
+            final RecyclerView rvPhotos = viewHolder.getView(R.id.rv_photos);
+            // 重置 divider
+            rvPhotos.removeItemDecoration(divider);
+            rvPhotos.addItemDecoration(divider);
+            if (rvPhotos.getLayoutManager() == null)
+                rvPhotos.setLayoutManager(new GridLayoutManager(DetailedActivity.this, 3));
+            PhotosAdapter photosAdapter = new PhotosAdapter(
+                    DetailedActivity.this,
+                    R.layout.item_picture,
+                    item.second
+            );
+            photosAdapter.setOnItemClickListener(new OnItemClickListener() {
+                @Override
+                public void onItemClick(View view, RecyclerView.ViewHolder holder, int pos) {
+                    transferee.apply(getBuilder(position)
+                            .setNowThumbnailIndex(pos)
+                            .setSourceUrlList(item.second)
+                            .bindRecyclerView(((RecyclerView) view.getParent()), R.id.iv_thum)
+                    ).show();
+                }
+
+                @Override
+                public boolean onItemLongClick(View view, RecyclerView.ViewHolder viewHolder, int i) {
+                    return false;
+                }
+            });
+            rvPhotos.setAdapter(photosAdapter);
+        }
+    }
+
+    /**
+     * 单个 item 中照片数据适配器
+     */
+    private static class PhotosAdapter extends CommonAdapter<String> {
+
+        PhotosAdapter(Context context, int layoutId, List<String> datas) {
+            super(context, layoutId, datas);
+        }
+
+        @Override
+        protected void convert(final ViewHolder holder, String url, final int position) {
+            ImageView imageView = holder.getView(R.id.iv_thum);
+            Glide.with(imageView)
+                    .load(url)
+                    .placeholder(R.mipmap.ic_empty_photo)
+                    .into(imageView);
+        }
+    }
+    public static List<Pair<String, List<String>>> getFriendsCircleList(Context context) {
+        List<Pair<String, List<String>>> friendsCircleList = new ArrayList<>();
 
         List<String> imgs = new ArrayList<>();
         imgs.add("https://file06.16sucai.com/2016/0613/8b7ab7ea218d6fbea16d75eda49bd9ca.jpg");
@@ -59,20 +212,42 @@ public class DetailedActivity extends AppCompatActivity implements View.OnClickL
         imgs.add("https://file06.16sucai.com/2016/0613/732a0311b4d49b20d7d9d904f07a5357.jpg");
         imgs.add("https://file06.16sucai.com/2016/0613/1c5d376864fb8fa320b624b066078658.jpg");
         imgs.add("http://pic1.bbzhi.com/fengjingbizhi/ganshoudaziran-renyuziranzhutisheying/nature_sz197_people_nature_01_8780_11.jpg");
+        friendsCircleList.add(new Pair<>("当 enableHideThumb = false 时，表示 transferee 打开或关闭不会干扰原缩略图的显示隐藏，默认开启", imgs));
 
-        multiImageView.setList(imgs);
-
-        //点赞功能
-        mGoodView = new GoodView(this);
-        initView();
+        return friendsCircleList;
     }
+
+    /*@Override
+            protected void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.home_detailed);
+
+                //图片展示
+                multiImageView = findViewById(R.id.patient_detail_table_pictures);
+
+                imgs.add("https://file06.16sucai.com/2016/0613/8b7ab7ea218d6fbea16d75eda49bd9ca.jpg");
+                imgs.add("https://file06.16sucai.com/2016/0328/f6de184de1f109750ed5d316c3bbd324.jpg");
+                imgs.add("https://file06.16sucai.com/2016/0613/fcf84cb40747b3602135fb9dd4f0d897.jpg");
+                imgs.add("https://file06.16sucai.com/2016/0613/5b4aeec20404962438c8bb791f1979da.jpg");
+                imgs.add("https://file06.16sucai.com/2016/0613/892e575cf8ba89579eacde6fae3bcf74.jpg");
+                imgs.add("https://file06.16sucai.com/2016/0613/33445716589c16ca0742d1cefc2ac701.jpg");
+                imgs.add("https://file06.16sucai.com/2016/0613/732a0311b4d49b20d7d9d904f07a5357.jpg");
+                imgs.add("https://file06.16sucai.com/2016/0613/1c5d376864fb8fa320b624b066078658.jpg");
+                imgs.add("http://pic1.bbzhi.com/fengjingbizhi/ganshoudaziran-renyuziranzhutisheying/nature_sz197_people_nature_01_8780_11.jpg");
+
+            multiImageView.setList(imgs);
+            //点赞功能
+            mGoodView = new GoodView(this);
+            initView1();
+
+        }*/
     //点赞点击事件
     public void collection(View view) {
         ((ImageView) view).setImageResource(R.drawable.collection_checked);
         mGoodView.setTextInfo("收藏成功", Color.parseColor("#f66467"), 12);
         mGoodView.show(view);
     }
-    private void initView() {
+    private void initView1() {
 
         // 初始化评论列表
         comment_list = (ListView) findViewById(R.id.comment_list);
